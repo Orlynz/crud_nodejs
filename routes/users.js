b6b07263aad84c642725df57f58334978d4c3232
Secondta <idnssquad300@gmail.com>
2022-02-09 10:17:17 +0700

    crud

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+var connection = require("../library/database");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  //query
+  connection.query("select * from users", function (err, rows) {
+    res.render("users", { data: rows });
+  });
+});
+
+module.exports = router;
